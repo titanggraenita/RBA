946c9364eee8217d2a63368f92b9cab6310132ae
ekharisma <ekharisma@outlook.com>
2021-10-27 09:17:28 +0700

    fix password-reset

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string("password");
             $table->string("role")->nullable();
             $table->string("status")->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
